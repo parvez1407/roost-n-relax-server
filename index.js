60bbdbdc07341547c670d036bcae9fbc31a1f34b
parvez1407 <info.nahidwd@gmail.com>
2023-01-26 01:38:06 +0600

    Ready Server Uploded

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktnfrsc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+  try {
+    const homesCollection = client.db('roostNRelax').collection('homes')
+    const usersCollection = client.db('roostNRelax').collection('users')
+
+    // save user email & generate JWT
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result)
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      })
+      console.log(token)
+      res.send({ result, token })
+    })
+    console.log('Database Connected...')
+  } finally {
+  }
+}
+
+run().catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('roost-n-relax Server is running...')
+})
+
+app.listen(port, () => {
+  console.log(`roost-n-relax Server is running...on ${port}`)
+})
